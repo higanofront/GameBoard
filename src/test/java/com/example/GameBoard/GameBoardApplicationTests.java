d16fb1436665508bed1f5b3112630fe2bce84e51
higanofront <higanofront@yahoo.co.jp>
2021-01-12 08:14:22 +0900

    second commit

---
+++ src/test/java/com/example/GameBoard/GameBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GameBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
